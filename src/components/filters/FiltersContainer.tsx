132b908106f9f773f71a9cef387207d8517d9706
leandevesa-despegar <leandevesa@gmail.com>
2019-04-21 13:12:56 -0300

    responsiveness

--- src/components/filters/FiltersContainer.tsx
+++ src/components/filters/FiltersContainer.tsx
@@ -8,7 +8,15 @@ class FiltersContainer extends Component {
     render() {
       return (
         <div className="col-12 col-md-3 col-lg-2 col-xl-2 filters-container">
-          <nav>
+          <div className="mobile">
+            <span className="label">Filtros</span>
+            <button className="btn btn-link" type="button">
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" role="img" focusable="false">
+                <path stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="2" d="M4 7h22M4 15h22M4 23h22"></path>
+              </svg>
+            </button>
+          </div>
+          <nav className="filters-nav">
               <Toggle></Toggle>
               <Spinner 
                 label="Precio hasta:"
